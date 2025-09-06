ac809b1f5b8f7e345794275c6c55120e07abe864
IHaveQuestions2010 <petivagyok2010@gmail.com>
2025-09-06 15:22:12 +0200

    Made basic team selector using cookies

--- app/app.py
+++ app/app.py
@@ -1,18 +1,25 @@
-from uppgifter import uppgifter
+from uuids import uuids
 
 from flask import Flask, render_template, abort, request
 app = Flask(__name__)
+
+teams = ["a", "b", "c"]
 
 @app.route('/')
 def hello():
 	return "Hello World!"
 
+@app.route('/login')
+def login():
+	return render_template('login.html', len = len(teams), teams = teams)
+
+
 @app.route('/qr')
 def qr():
 	id = request.args.get('id')
-	if not id or id not in uppgifter: abort(400, 'Invalid ID')
+	if not id or id not in uuids: abort(400, 'Invalid ID')
 	print(f'{id=}')
-	return render_template('qr.html', uppgift=uppgifter[id])
+	return render_template('qr.html', uuid=uuids[id])
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=6969)
--- app/uuids.py
+++ app/uuids.py
@@ -1,4 +1,4 @@
-uppgifter = {
+uuids = {
     '16c174ee-4f7c-4ece-9a6e-e09d4e60c0ec': 'Plats 1',
     '2c00e7b1-4e6c-43a6-84ad-f47a4361722b': 'Plats 2',
     '62d86557-968a-48ec-9844-4d2da5fc9197': 'Plats 3',

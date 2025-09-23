a2cce561384b6d1d63d9c2b9ad0844752b5d34e5
IHaveQuestions2010 <petivagyok2010@gmail.com>
2025-09-23 19:21:33 +0200

    Made each team have a order for the rooms and updated the /qr route

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 import os
-import importlib
 import json
+import random
 
 from flask import Flask, render_template, abort, request, redirect, url_for
 app = Flask(__name__)
@@ -10,18 +10,18 @@ basedir = os.path.dirname(__file__)
 problems_path = os.path.join(basedir, 'problems')
 statefile_path = os.path.join(basedir, 'user', 'statefile.json')
 configfile_path = os.path.join(basedir, 'user', 'config.json')
-logfile_path = os.path.join('user', 'logfile.log')
+logfile_path = os.path.join(basedir, 'user', 'logfile.log')
+orderfile_path = os.path.join(basedir, 'user', "order.json")
 
-problems = [p for p in os.listdir(
-    problems_path) if os.path.isdir(os.path.join(problems_path, p))]
-
-print('Problems', problems)
 
 with open(configfile_path, 'r') as config_file:
     config_data = json.load(config_file)
+    problems = config_data['problems']
     teams = config_data['teams']
     uuids = [config_data['rooms'][i]['uuid']
              for i in range(len(config_data['rooms']))]
+
+print('Problems', problems)
 
 
 # def print(*msg):
@@ -36,7 +36,7 @@ with open(configfile_path, 'r') as config_file:
 #                 pass
 
 
-if os.path.exists("statefile_path"):
+if os.path.exists(statefile_path):
     with open(statefile_path) as statefile:
         team_state = json.loads(statefile.read())
 else:
@@ -52,9 +52,25 @@ def storeState():
         statefile.write(json.dumps(team_state, indent=4))
 
 
+random.seed(config_data['seed'])
+
+if os.path.exists(orderfile_path):
+    with open(orderfile_path) as orderfile:
+        team_order = json.loads(orderfile.read())
+else:
+    team_order = {}
+    for team in teams:
+        team_order.update({team: random.sample(uuids, len(uuids))})
+
+    with open(orderfile_path, 'w') as orderfile:
+        orderfile.write(json.dumps(team_order, indent=4))
+
+
 @app.route('/')
 def hello():
     if 'team' not in request.cookies:
+        return redirect(url_for('login'), code=307)
+    elif request.cookies.get('team') not in teams:
         return redirect(url_for('login'), code=307)
     else:
         return render_template('index.html')
@@ -66,51 +82,21 @@ def login():
     return render_template('login.html', len=len(teams), teams=list(teams))
 
 
+# TODO: Implement showing problems and being able to solve them
 @app.route('/qr')
 def qr():
-    id = request.args.get('id')
+    problem_id = request.args.get('id')
     team = request.cookies.get('team')
     if team not in teams:
         return redirect('/login')
-    if not id or id not in uuids:
+    if not problem_id or problem_id not in uuids:
         return abort(400, 'Invalid ID')
-    print(f'{id=}, {team=}, {team[team_state[team]]=}')
 
-    if team[team_state[team]] == id:
-        team_state[team] += 1
-        storeState()
-    if team_state[team] > 0 and team[team_state[team]-1] == id:
-        pass
-    else:
-        return abort(401, 'Fel QR')
+    if team_order[team][team_state[team]] != problem_id:
+        return render_template('404.html'), 404
 
     problem = problems[team_state[team]]
-
-    print(f'{problem=}')
-
     return render_template('problem.html', problem=problem)
-
-
-@app.route('/problem')
-def problem():
-    problem = request.args.get('problem')
-    if not problem or problem not in problems:
-        abort(400, 'Invalid ID')
-    id = request.args.get('id')
-    if not id or id not in uuids:
-        return abort(400, 'Invalid ID')
-    flag = uuids[id]
-    print(f'{problem=}')
-    problemModule = importlib.import_module(
-        f'problem.{problems[problems.index(problem)]}', package=None)
-    if getattr(problemModule, 'generateFile', None):
-        return render_template('problem.html', filename=problem)
-    elif getattr(problemModule, 'generateText', None):
-        return render_template('problem.html', text=problemModule.generateText(flag))
-    elif getattr(problemModule, 'generateImage', None):
-        return render_template('problem.html', image='image')
-    else:
-        return abort(500, 'Invalid problem')
 
 
 if __name__ == '__main__':

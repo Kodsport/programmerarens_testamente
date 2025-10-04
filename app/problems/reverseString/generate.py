0cb96d6eb90680415c5303ab628972dbd699d92d
IHaveQuestions2010 <petivagyok2010@gmail.com>
2025-10-05 00:04:49 +0200

    Added a few more problems

---
+++ app/problems/reverseString/generate.py
@@ -0,0 +1,9 @@
+def generateCode(flag: str):
+    kod = f"""
+s = {''.join(reversed(flag))}
+print(''.join(reversed(s)))
+"""
+    return kod
+
+if __name__ == '__main__':
+    print(generateCode('AB123'))
